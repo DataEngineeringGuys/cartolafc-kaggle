bdcfa09a3edd52e3c34386ce0d7d9545611563d1
yuri.fialho <yuri.fialho@yandeh.com.br>
2018-02-23 13:01:27 -0300

    add filename to args

--- cartolafc-kaggle-sandbox/src/main/scala/data/engineering/guys/CartolaAthletesSparkApp.scala
+++ cartolafc-kaggle-sandbox/src/main/scala/data/engineering/guys/CartolaAthletesSparkApp.scala
@@ -7,7 +7,7 @@ object CartolaAthletesSparkApp {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("Spark SQL basic example").getOrCreate()
 
-    val df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("D:\\projects\\cartolafc-kaggle\\data\\2014_atletas.csv")
+    val df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(args(0))
     df.show()
   }
 }
