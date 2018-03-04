18762a746f42aeab8b9be126374d949c21f3b52d
Anderson Ito <andersonkmi@acm.org>
2018-03-04 19:40:20 -0300

    Configured project to run as standalone program

--- cartolafc-kaggle-sandbox/src/main/scala/data/engineering/guys/CartolaAthletesSparkApp.scala
+++ cartolafc-kaggle-sandbox/src/main/scala/data/engineering/guys/CartolaAthletesSparkApp.scala
@@ -2,10 +2,9 @@ package data.engineering.guys
 
 import org.apache.spark.sql.SparkSession
 
-
 object CartolaAthletesSparkApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("Spark SQL basic example").getOrCreate()
+    val spark = SparkSession.builder().appName("Spark SQL basic example").master("local[*]").getOrCreate()
 
     val df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(args(0))
     df.show()
